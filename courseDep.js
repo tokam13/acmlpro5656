241cb736f379cc887430b556ce2ff169460ca058
sarawaleed <74360448+sarawaleed@users.noreply.github.com>
2020-12-17 13:28:29 +0200

    deleted

--- courseDep.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-const courseDep=mongoose.Schema({
-      departmentName:{type:String,required:true},
-      courseName:{type:String,required:true}
-})
-module.exports=mongoose.model('courseDep',courseDep);
