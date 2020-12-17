6a1342e05a1a824715dce81d9bde497c938c28b4
sarawaleed <74360448+sarawaleed@users.noreply.github.com>
2020-12-17 23:35:35 +0200

    beforetest

---
+++ routes/hr.js
@@ -0,0 +1,36 @@
+const express=require('express');
+const router=express.Router();
+const staffMembers=require('../models/staffMembers');
+const location=require('../models/locations')
+const faculty=require('../models/faculty')
+//router.route('/:location')
+
+router.route('/:location')
+.post(async(req,res)=>{
+    const loc={
+        location:req.params.location,
+        remainingPlaces:req.params.remainingPlaces,
+        capacity:req.params.capacity,
+        type:req.params.type
+    }
+    await location.insertOne(loc);
+    console.log("location inserted");
+})
+.delete(async(req,res)=>{
+    const result=await location.findOneAndDelete({"locaton":req.params.location})
+    if(result!=null){
+        res.send(result)
+    }
+    else{
+        res.send("there is no data for this location")
+    }
+})
+.put(async(req,res)=>{
+ 
+    res.send("enter the update ");
+    
+    const result= await location.findOneAndUpdate({"location":req.params.location},req.body,{new:true})
+    res.send(result);
+})
+
+
